83aade682b9f8ae358afad5b6a2c37237b575401
Thiago Figueira <tsantosfigueira@outlook.com>
2017-05-18 13:01:25 -0400

    version 1.0
    Object Pool and Pool Manager are now concluded;

---
+++ Assets/InterfacePool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfacePool : MonoBehaviour {
+
+    public string poolName;
+    public GameObject poolObject;
+    public int poolSize;
+
+    // Use this for initialization
+	void Start () {
+        ObjectPoolingManager.Instance.CreatePool(poolObject, poolSize, 10, transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject bullet = ObjectPoolingManager.Instance.GetObject("Cube");
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = Quaternion.identity;
+            bullet.SetActive(true);
+        }
+	}
+
+
+}
--- Assets/ObjectsPool.cs
+++ Assets/ObjectsPool.cs
@@ -1,50 +1,69 @@
-﻿using System;
+﻿/* 
+ * Use the object pools to help reduce object instantiation time and performance 
+ * with objects that are frequently created and used. 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class ObjectsPool : MonoBehaviour {
+/// <summary>
+/// The object pool is a list of already instantiated game objects of the same type.
+/// </summary>
+public class ObjectsPool
+{
 
-    GameObject objectToPool;         // objeto original
-    int amountToPool;                // quantidade total de objetos a serem gerados
-    bool canGrow = false;            // pode aumentar em tempo de execução?
-    List<GameObject> pooledObjects;  // lista de objetos   
+    private List<GameObject> objectsPool; //!< List of pooled objects
+    private GameObject originalObject;    //!< Reference to the original object to pool
+    private int initialPoolSize;          //!< Default and initial pool size
+    private int maxPoolSize;              //!< Maximum pool size
+    private Transform parent;             //!< Parent reference to allocate instantiated objects
 
-    public ObjectsPool(GameObject obj, int amount, bool grows)
+    /// <summary>
+    /// Constructor for creating a new Object Pool.
+    /// </summary>
+    /// <param name="objectToPool">Game Object for this pool</param>
+    /// <param name="initialPoolSize">Initial and default size of the pool.</param>
+    /// <param name="maxPoolSize">Maximum number of objects this pool can contain.</param>
+    /// <param name="parent">Where to instantiate game objects</param>
+    public ObjectsPool(GameObject objectToPool, int initialPoolSize, int maxPoolSize, Transform parent)
     {
-        objectToPool = obj;
-        amountToPool = amount;
-        canGrow = grows;
+        objectsPool = new List<GameObject>();
+
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject auxiliaryObject = GameObject.Instantiate(objectToPool, parent) as GameObject;
+            auxiliaryObject.SetActive(false);
+            objectsPool.Add(auxiliaryObject);
+        }
+
+        this.originalObject = objectToPool;
+        this.initialPoolSize = initialPoolSize;
+        this.maxPoolSize = maxPoolSize;
+        this.parent = parent;
     }
-    
-    void Start () {
 
-        // cria a qtde de objetos especificada
-        pooledObjects = new List<GameObject>();  
-        for (int i = 0; i < amountToPool; i++)
-        {
-            GameObject obj = Instantiate(objectToPool, gameObject.transform);
-            obj.SetActive(false);
-            pooledObjects.Add(obj);
-        }
-	}
-	
-    public GameObject getBullet ()
+    /// <summary>
+    /// Returns an active object from the object pool without resetting any of its values.
+    /// You will need to set its values and set it inactive again when you are done with it.
+    /// </summary>
+    /// <returns>Game Object of requested type if it is available, otherwise null.</returns>
+    public GameObject GetObject()
     {
-        for (int i = 0; i < pooledObjects.Count; i++)
+        for (int i = 0; i < objectsPool.Count; i++)
         {
-            if (!pooledObjects[i].activeInHierarchy)
-                return pooledObjects[i];
-        }
+            if (!objectsPool[i].activeSelf)
+            {
+                return objectsPool[i];
+            }
 
-        if (canGrow)
-        {
-            GameObject obj = (GameObject)Instantiate(objectToPool);
-            pooledObjects.Add(obj);
-            return obj;
+            if (this.maxPoolSize > this.objectsPool.Count)
+            {
+                GameObject auxiliaryObject = GameObject.Instantiate(this.originalObject, this.parent) as GameObject;
+                objectsPool.Add(auxiliaryObject);
+                return auxiliaryObject;
+            }
         }
-
         return null;
     }
 }

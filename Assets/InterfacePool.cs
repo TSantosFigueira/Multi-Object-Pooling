a662e245663d2186981b76768c68ce666c4f4a33
Thiago Figueira <tsantosfigueira@outlook.com>
2017-05-22 15:40:18 -0400

    version 1.0.1
    Small fix for the Pool Interface script.

--- Assets/InterfacePool.cs
+++ Assets/InterfacePool.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class InterfacePool : MonoBehaviour {
 
-    public string poolName;
     public GameObject poolObject;
     public int poolSize;
+    public int maxPoolSize;
 
-    // Use this for initialization
 	void Start () {
-        ObjectPoolingManager.Instance.CreatePool(poolObject, poolSize, 10, transform);
+        ObjectPoolingManager.Instance.CreatePool(poolObject, poolSize, maxPoolSize, transform);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Jump"))
-        {
-            GameObject bullet = ObjectPoolingManager.Instance.GetObject("Cube");
-            bullet.transform.position = transform.position;
-            bullet.transform.rotation = Quaternion.identity;
-            bullet.SetActive(true);
-        }
-	}
+
+    // Retrieve object from pool example
+    void Update()
+    {
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    GameObject bullet = ObjectPoolingManager.Instance.GetObject("Cube");
+        //    bullet.transform.position = transform.position;
+        //    bullet.transform.rotation = Quaternion.identity;
+        //    bullet.SetActive(true);
+        //}
+    }
 
 
 }
